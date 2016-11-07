73650eaa70535ca56a4b63ca07188020c82570db
techdeewa <techdeewa@outlook.com>
2016-11-07 22:21:58 +0700

    Update bot.php
    fiex

--- bot.php
+++ bot.php
@@ -10,16 +10,13 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'source') {
-		
-			$userId = $event['source']['userId'];
-		}
 		
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 
+			$userId = $event['source']['userId'];
 			
 			$replyToken = $event['replyToken'];
 
