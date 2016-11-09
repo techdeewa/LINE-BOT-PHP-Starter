328a990bd26c7ef91ce1790b7ca437cc89b938d1
techdeewa <techdeewa@outlook.com>
2016-11-09 10:38:04 +0700

    Update loadcsv.php
    view eng

--- loadcsv.php
+++ loadcsv.php
@@ -6,8 +6,12 @@ while (!feof($file_handle) ) {
 
 $line_of_text = fgetcsv($file_handle, 874);
 
-print $line_of_text[0] . $line_of_text[1]. $line_of_text[2] . "<BR>";
-
+print $line_of_text[0] . "<BR>";
+print $line_of_text[2] . "<BR>";
+print $line_of_text[3] . "<BR>";
+print $line_of_text[4] . "<BR>";
+print $line_of_text[5] . "<BR>";
+print "======================================================================" . "<BR>";  
 }
 
 fclose($file_handle);
