2a1f5709df1892679b4afd7ebb2f2146dafbfc5d
techdeewa <techdeewa@outlook.com>
2016-11-09 10:40:14 +0700

    Update loadcsv.php
    rearrange

--- loadcsv.php
+++ loadcsv.php
@@ -6,11 +6,11 @@ while (!feof($file_handle) ) {
 
 $line_of_text = fgetcsv($file_handle, 874);
 
-print $line_of_text[0] . "<BR>";
-print $line_of_text[2] . "<BR>";
-print $line_of_text[3] . "<BR>";
-print $line_of_text[4] . "<BR>";
-print $line_of_text[5] . "<BR>";
+print "FUND : " . $line_of_text[0] . "<BR>";
+print "NAME : " . $line_of_text[2] . "<BR>";
+print "CODE : " . $line_of_text[3] . "<BR>";
+print "ASSET : " . $line_of_text[4] . "<BR>";
+print "NAV  " . $line_of_text[5] . "<BR>";
 print "======================================================================" . "<BR>";  
 }
 
