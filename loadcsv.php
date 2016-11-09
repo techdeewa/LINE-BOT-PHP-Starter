af9eb4fb8cd6a70c825ba0370238479bdd87d460
techdeewa <techdeewa@outlook.com>
2016-11-09 10:35:16 +0700

    Update loadcsv.php
    codepage 874

--- loadcsv.php
+++ loadcsv.php
@@ -4,7 +4,7 @@ $file_handle = fopen("mutualfund.csv", "r");
 
 while (!feof($file_handle) ) {
 
-$line_of_text = fgetcsv($file_handle, 1024);
+$line_of_text = fgetcsv($file_handle, 874);
 
 print $line_of_text[0] . $line_of_text[1]. $line_of_text[2] . "<BR>";
 
