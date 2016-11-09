6d853ba10e1e65f9292cebcb002d30f4c8bacaa2
techdeewa <techdeewa@outlook.com>
2016-11-09 10:45:35 +0700

    Update loadcsv.php
    filter

--- loadcsv.php
+++ loadcsv.php
@@ -6,12 +6,15 @@ while (!feof($file_handle) ) {
 
 $line_of_text = fgetcsv($file_handle, 874);
 
-print "FUND : " . $line_of_text[0] . "<BR>";
-print "NAME : " . $line_of_text[2] . "<BR>";
-print "CODE : " . $line_of_text[3] . "<BR>";
-print "ASSET : " . $line_of_text[4] . "<BR>";
-print "NAV  " . $line_of_text[5] . "<BR>";
-print "======================================================================" . "<BR>";  
+  if ($_GET['code'] == $line_of_text[3] )
+  {
+    print "FUND : " . $line_of_text[0] . "<BR>";
+    print "NAME : " . $line_of_text[2] . "<BR>";
+    print "CODE : " . $line_of_text[3] . "<BR>";
+    print "ASSET : " . $line_of_text[4] . "<BR>";
+    print "NAV  " . $line_of_text[5] . "<BR>";
+    print "======================================================================" . "<BR>";  
+  }
 }
 
 fclose($file_handle);
