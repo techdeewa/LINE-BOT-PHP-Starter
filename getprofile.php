779dedb48a199a2c54a936bc920865176d027945
techdeewa <techdeewa@outlook.com>
2016-11-09 11:23:09 +0700

    Create getprofile.php
    get name

---
+++ getprofile.php
@@ -0,0 +1,17 @@
+<?php
+$access_token = '9nrLlQp7Hd55PKTI20j0A/StX1fbO8MWQblh2Jf0dNc+uN7AjvI13IAjnTgGgfPPn2DGKE2lPTfvw2odlJBa/MQYZyDE7Mu1U0xbHUGFyru6n3AcOogiLlCeKOIKk3UQr83A9odZMo+N0eKf8/2migdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v2/bot/profile/Ufe79d34680fcd7c2cf50f61c797c3931';
+
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			  $ch = curl_init($url);        
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);        
+        $output = curl_exec($ch);       
+        curl_close($ch);
+        echo $output;
+?>
