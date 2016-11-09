88177394a9998d1e4ee59631fe4d4db85f10bd6d
techdeewa <techdeewa@outlook.com>
2016-11-09 11:25:40 +0700

    Update getprofile.php
    fixed

--- getprofile.php
+++ getprofile.php
@@ -5,12 +5,13 @@ $url = 'https://api.line.me/v2/bot/profile/Ufe79d34680fcd7c2cf50f61c797c3931';
 
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			  $ch = curl_init($url);        
-        curl_setopt($ch, CURLOPT_HEADER, 0);
+	$ch = curl_init($url);        
+        //curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);        
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);        
         $output = curl_exec($ch);       
         curl_close($ch);
         echo $output;
