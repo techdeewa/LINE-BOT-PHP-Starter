c36c686b5ceeb7acd012b54b1c5d33d50fae7aa7
techdeewa <techdeewa@outlook.com>
2016-12-28 15:21:35 +0700

    Update query.php
    x

--- query.php
+++ query.php
@@ -36,11 +36,11 @@ $tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=%23ร
 
     ====>Split: <?php
       $segment = new Segment();
-echo "test";
+
       $result = $segment->get_segment_array($tweet->text);
-echo "test2";                                                     
+
       echo implode('|', $result);
-echo "test3";                                                     
+
     ?><br>
     <hr/>
   
