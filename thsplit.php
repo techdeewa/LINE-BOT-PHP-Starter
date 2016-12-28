31bcc43726cf1c66b1523f09b11f030053990c21
techdeewa <techdeewa@outlook.com>
2016-12-28 14:45:29 +0700

    Update thsplit.php
    x

--- thsplit.php
+++ thsplit.php
@@ -5,10 +5,5 @@ include(dirname(FILE) . DIRECTORY_SEPARATOR . 'THSplitLib/segment.php');
 
 echo "Start";
 
-$segment = new Segment();
-
-$result = $segment->get_segment_array("คำที่ต้องการตัด");
-
-echo implode(' | ', $result);
 
 ?>
