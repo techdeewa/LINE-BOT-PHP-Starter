e669d6a825a951cfd2d77db3f42b4b693dbae6f9
techdeewa <techdeewa@outlook.com>
2016-12-28 16:16:56 +0700

    Update query.php
    ป

--- query.php
+++ query.php
@@ -4,6 +4,11 @@
  * Selim Hallaç
  * selimhallac@gmail.com
  */
+
+//https://dev.twitter.com/rest/reference/get/search/tweets
+//https://mods.me/
+//https://github.com/techdeewa/thsplitlib
+
 include "twitteroauth.php";
 include(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'THSplitLib/segment.php');
 
@@ -11,8 +16,7 @@ $consumer_key = "pfIvJnT5DOhCsaHUWUy51Aoa4";
 $consumer_secret = "CFPK5eLYOwVKnA4WrODtwPolNxb46OiuyYKCLez0bqIBL2Hrdq";
 $access_token = "4364745913-X7KqrHZzwqHyAOMS6u41k0OgghJkeUJlDBcYNM0";
 $access_token_secret = "LsCT8obowyooAUBtL2DIz2UtnX3Uv1jPYGw3Qqb4WQfIo";
-$twitter = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
-$tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=%23ร่างใหม่หัวใจเดิม&count=500');
+
 //%40 = @
 //%23 = #
 ?>
@@ -23,29 +27,34 @@ $tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=%23ร
   <title>Twitter API SEARCH</title>
 </head>
 <body>
+<form action="" method="post">
+  <label>Search : <input type="text" name="keyword" value="<?=$_POST["keyword"]?>"/></label>
+  <br/>
+  <?= urlencode($_POST["keyword"]);?>
+  <hr/>
+</form>
 <?php
 
-//var_dump($tweets);
+if (isset($_POST["keyword"])){
 
+  //var_dump($tweets);
+  $twitter = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
+  $tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q='.urlencode($_POST["keyword"]).'&result_type=recent&count=50');
 ?>
-<?php foreach ($tweets->statuses as $key => $tweet) { ?>
-    <img src="<?=$tweet->user->profile_image_url;?>" /><br/>
-    <?=$tweet->user->name;?>(@<?=$tweet->user->screen_name;?>)<br>
-    <?=$tweet->text; ?><br>
-    <!--<?=$tweet->text; ?><br> -->
+    <?php foreach ($tweets->statuses as $key => $tweet) { ?>
+        <img src="<?=$tweet->user->profile_image_url;?>" /><br/>
+        <?=$tweet->user->name;?>(@<?=$tweet->user->screen_name;?>)<br>
+        <?=$tweet->text; ?><br>
+        <!--<?=$tweet->text; ?><br> -->
 
-    ====>Split: <?php
-      $segment = new Segment();
-
-      $result = $segment->get_segment_array($tweet->text);
-
-      echo implode('|', $result);
-
-    ?><br>
-    <hr/>
-  
+        ====>Split: <?php
+          $segment = new Segment();
+          $result = $segment->get_segment_array($tweet->text);
+          echo implode('|', $result);
+        ?><br>
+        <hr/>
+    <?php } ?>
 <?php } ?>
-
 
 </body>
 </html>
